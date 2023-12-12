dbae93d1d56720d57cb2a6f2362f9992296447ff
Richard Bond <richardbond@Richards-MBP.gt.rr.com>
2023-12-11 20:41:03 -0600

    added for loop

--- index.go
+++ index.go
@@ -1,7 +1,30 @@
 package main
-import "fmt"
+import (
+    "fmt"
+)
 
-func main (){
+func typesPlay () {
+    // number
+    var num int = 12
+    fmt.Println(num)
+
+    // string
+    var myString = "myString"
+    fmt.Println(myString)
+
+    // string methods
+
+}
+
+func forLoop (){
+    for i := 1; i <= 10; i++ {
+        fmt.Println("i = ", i)
+    }
+}
+
+func main () {
     fmt.Println("hello world")
+    typesPlay()
+    forLoop()
 
 }
